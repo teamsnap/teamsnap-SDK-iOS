716d5f21eda439d144c8412c52116234fcbcab14
Jason Rahaim <jason@jasonr.com>
2015-09-18 13:44:28 -0400

    Update memebers with Statistics links

--- TeamSnapSDK/SDK/DataTypes/TSDKMember.h
+++ TeamSnapSDK/SDK/DataTypes/TSDKMember.h
@@ -2,63 +2,73 @@
 //  TSDKRoster.h
 //  SDKPlayground
 //
-//  Created by Jason Rahaim on 1/31/15.
-//  Copyright (c) 2015 TeamSnap. All rights reserved.
+// Copyright (c) 2015 TeamSnap. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
 #import "TSDKCollectionObject.h"
+#import "TSDKObjectsRequest.h"
 
 @interface TSDKMember : TSDKCollectionObject
 
-
-@property (nonatomic, weak) NSDate *lastLoggedInAt; //Example: 2015-03-19T12:26:11Z
-@property (nonatomic) BOOL isInvitable; //Example: <null>
-@property (nonatomic) BOOL isNonPlayer; //Example: 0
-@property (nonatomic, weak) NSDate *birthday; //Example: 2003-09-07
+@property (nonatomic) BOOL isInvited; //Example: 0
+@property (nonatomic) BOOL isInvitable; //Example: 0
+@property (nonatomic, weak) NSString *birthday; //Example: 1967-05-12
 @property (nonatomic) NSInteger hideAge; //Example: 0
-@property (nonatomic, weak) NSString *jerseyNumber; //Example: 10
-@property (nonatomic, weak) NSString *addressCity; //Example: Willow Spring
-@property (nonatomic, weak) NSString *invitationCode; //Example: **NULL**
-@property (nonatomic, weak) NSString *addressZip; //Example: 27592
-@property (nonatomic, weak) NSString *position; //Example: 1st, pitcher
-@property (nonatomic) NSInteger teamId; //Example: 153832
-@property (nonatomic) NSInteger userId; //Example: 581732
-@property (nonatomic) BOOL isOwner; //Example: 1
+@property (nonatomic) BOOL isNonPlayer; //Example: 1
+@property (nonatomic, weak) NSString *jerseyNumber; //Example: 19
+@property (nonatomic, weak) NSString *addressCity; //Example: Fuquay-Varina
+@property (nonatomic, weak) NSString *invitationCode; //Example: r53e4a619e54e39f2d93e14d2
+@property (nonatomic, weak) NSString *addressZip; //Example: 27526
+@property (nonatomic, weak) NSDate *lastLoggedInAt; //Example: 2015-09-16T22:34:28Z
+@property (nonatomic, weak) NSString *position; //Example:
+@property (nonatomic) NSInteger teamId; //Example: 71118
 @property (nonatomic, weak) NSString *addressState; //Example: NC
-@property (nonatomic, weak) NSDate *updatedAt; //Example: 2015-03-19T12:26:11Z
-@property (nonatomic, weak) NSString *lastName; //Example: Cargile
+@property (nonatomic) BOOL isOwner; //Example: 1
+@property (nonatomic) NSInteger userId; //Example: 388192
+@property (nonatomic, weak) NSDate *updatedAt; //Example: 2015-09-16T22:34:28Z
+@property (nonatomic, weak) NSString *lastName; //Example: R
+@property (nonatomic) BOOL hasFacebookPostScoresEnabled; //Example: 1
 @property (nonatomic) BOOL isOwnershipPending; //Example: <null>
-@property (nonatomic) NSInteger hideAddress; //Example: 1
+@property (nonatomic) NSInteger hideAddress; //Example: 0
 @property (nonatomic, weak) NSString *invitationDeclined; //Example: **NULL**
-@property (nonatomic, weak) NSString *addressStreet1; //Example: 7109 Pekin Drive
+@property (nonatomic, weak) NSString *addressStreet1; //Example: 716 Papsworth Ct.
 @property (nonatomic, weak) NSString *gender; //Example: Male
-@property (nonatomic, weak) NSDate *createdAt; //Example: 2013-03-19T22:28:03Z
+@property (nonatomic, weak) NSDate *createdAt; //Example: 2012-03-25T23:46:02Z
 @property (nonatomic, weak) NSString *addressStreet2; //Example: **NULL**
-@property (nonatomic, weak) NSString *firstName; //Example: Caden
-@property (nonatomic) BOOL isActivated; //Example: <null>
+@property (nonatomic, weak) NSString *firstName; //Example: Jason
+@property (nonatomic) BOOL isActivated; //Example: 1
 @property (nonatomic) BOOL isManager; //Example: 1
 @property (nonatomic, weak) NSURL *linkBroadcastEmails;
-@property (nonatomic, weak) NSURL *linkMemberPreferences;
+@property (nonatomic, weak) NSURL *linkBroadcastEmailAttachments;
 @property (nonatomic, weak) NSURL *linkBroadcastSmses;
+@property (nonatomic, weak) NSURL *linkMemberLinks;
+@property (nonatomic, weak) NSURL *linkMemberPreferences;
 @property (nonatomic, weak) NSURL *linkTeam;
-@property (nonatomic, weak) NSURL *linkMemberPhoto;
 @property (nonatomic, weak) NSURL *linkMemberPhoneNumbers;
+@property (nonatomic, weak) NSURL *linkMemberPhoto;
 @property (nonatomic, weak) NSURL *linkMemberEmailAddresses;
+@property (nonatomic, weak) NSURL *linkStatisticData;
 @property (nonatomic, weak) NSURL *linkForumSubscriptions;
+@property (nonatomic, weak) NSURL *linkLeagueCustomData;
 @property (nonatomic, weak) NSURL *linkContactPhoneNumbers;
 @property (nonatomic, weak) NSURL *linkContactEmailAddresses;
+@property (nonatomic, weak) NSURL *linkTeamMedia;
 @property (nonatomic, weak) NSURL *linkMemberThumbnail;
 @property (nonatomic, weak) NSURL *linkForumTopics;
-@property (nonatomic, weak) NSURL *linkTrackedItemStatuses;
+@property (nonatomic, weak) NSURL *linkTeamMediumComments;
 @property (nonatomic, weak) NSURL *linkCustomFields;
 @property (nonatomic, weak) NSURL *linkAssignments;
 @property (nonatomic, weak) NSURL *linkCustomData;
-@property (nonatomic, weak) NSURL *linkUser;
+@property (nonatomic, weak) NSURL *linkMemberStatistics;
 @property (nonatomic, weak) NSURL *linkAvailabilities;
+@property (nonatomic, weak) NSURL *linkMemberBalances;
+@property (nonatomic, weak) NSURL *linkTrackedItemStatuses;
+@property (nonatomic, weak) NSURL *linkUser;
 @property (nonatomic, weak) NSURL *linkForumPosts;
 @property (nonatomic, weak) NSURL *linkMemberPayments;
-@property (nonatomic, weak) NSURL *linkMemberLinks;
+@property (nonatomic, weak) NSURL *linkLeagueCustomFields;
+@property (nonatomic, weak) NSURL *linkLeagueRegistrantDocuments;
 @property (nonatomic, weak) NSURL *linkContacts;
 @property (nonatomic, weak) NSURL *linkMemberFiles;
 
