5e60ba4ba94cbef330a0e8f3fe87332d768f1ba3
Jason Rahaim <jason@jasonr.com>
2015-09-19 11:55:27 -0400

    Added tvOS support. No binary yet

---
+++ TeamSnapSDKTV/TeamSnapSDKTV.h
@@ -0,0 +1,44 @@
+//
+//  TeamSnapSDKTV.h
+//  TeamSnapSDKTV
+//
+//  Created by Jason Rahaim on 9/19/15.
+//  Copyright © 2015 teamsnap. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TeamSnapSDKTV.
+FOUNDATION_EXPORT double TeamSnapSDKTVVersionNumber;
+
+//! Project version string for TeamSnapSDKTV.
+FOUNDATION_EXPORT const unsigned char TeamSnapSDKTVVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TeamSnapSDKTV/PublicHeader.h>
+
+#import <TeamSnapSDKTV/TSDKTeamSnap.h>
+#import <TeamSnapSDKTV/TSDKUser.h>
+#import <TeamSnapSDKTV/TSDKTeam.h>
+#import <TeamSnapSDKTV/TSDKEvent.h>
+#import <TeamSnapSDKTV/TSDKMember.h>
+#import <TeamSnapSDKTV/TSDKTrackedItem.h>
+#import <TeamSnapSDKTV/TSDKAssignment.h>
+#import <TeamSnapSDKTV/TSDKAvailability.h>
+#import <TeamSnapSDKTV/TSDKBroadcastEmail.h>
+#import <TeamSnapSDKTV/TSDKBroadcastSms.h>
+#import <TeamSnapSDKTV/TSDKCustomDatum.h>
+#import <TeamSnapSDKTV/TSDKCustomField.h>
+#import <TeamSnapSDKTV/TSDKForumPost.h>
+#import <TeamSnapSDKTV/TSDKForumTopic.h>
+#import <TeamSnapSDKTV/TSDKLocation.h>
+#import <TeamSnapSDKTV/TSDKmemberStatistic.h>
+#import <TeamSnapSDKTV/TSDKOpponent.h>
+#import <TeamSnapSDKTV/TSDKPlan.h>
+#import <TeamSnapSDKTV/TSDKSport.h>
+#import <TeamSnapSDKTV/TSDKStatistic.h>
+#import <TeamSnapSDKTV/TSDKStatisticDatum.h>
+#import <TeamSnapSDKTV/TSDKStatisticGroup.h>
+#import <TeamSnapSDKTV/TSDKTeamFee.h>
+#import <TeamSnapSDKTV/TSDKTeamPreferences.h>
+#import <TeamSnapSDKTV/TSDKTeamStatistic.h>
+#import <TeamSnapSDKTV/TSDKTrackedItemStatus.h>
