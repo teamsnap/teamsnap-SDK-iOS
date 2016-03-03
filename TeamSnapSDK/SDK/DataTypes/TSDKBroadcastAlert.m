30462596b54f51881d4abf097d55bedfdba8ceb9
Skyler Seamans <skyler.seamans@teamsnap.com>
2016-03-02 16:15:54 -0800

    Add convenience init for Broadcast Alert creation

--- TeamSnapSDK/SDK/DataTypes/TSDKBroadcastAlert.m
+++ TeamSnapSDK/SDK/DataTypes/TSDKBroadcastAlert.m
@@ -7,6 +7,7 @@
 //
 
 #import "TSDKBroadcastAlert.h"
+#import "TSDKMember.h"
 
 @implementation TSDKBroadcastAlert
 
@@ -14,6 +15,25 @@
 
 + (NSString *)SDKType {
     return @"broadcast_alert";
+}
+
+- (instancetype)initWithBody:(NSString *)body teamId:(NSInteger)teamId recipients:(NSArray <TSDKMember *>*)recipients sender:(TSDKMember *)sender {
+    self = [super init];
+    if(self) {
+        [super setString:body forKey:@"body"];
+        [super setInteger:teamId forKey:@"team_id"];
+        [super setInteger:sender.objectIdentifier forKey:@"member_id"];
+        NSMutableString *commaSeparatedMemberIds = [[NSMutableString alloc] init];
+        for(TSDKMember *recipient in recipients) {
+            if(recipient != recipients.lastObject) {
+                [commaSeparatedMemberIds appendString:[NSString stringWithFormat:@"%ld, ", (long)recipient.objectIdentifier]];
+            } else {
+                [commaSeparatedMemberIds appendString:[NSString stringWithFormat:@"%ld", (long)recipient.objectIdentifier]];
+            }
+        }
+        [super setString:commaSeparatedMemberIds forKey:@"recipient_ids"];
+    }
+    return self;
 }
 
 @end
