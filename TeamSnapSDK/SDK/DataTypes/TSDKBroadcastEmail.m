89308c3776e38f9b11577703b52b23a35a3ab95a
Skyler Seamans <skyler.seamans@teamsnap.com>
2016-03-31 20:39:43 -0700

    Added init method to broadcast emails

--- TeamSnapSDK/SDK/DataTypes/TSDKBroadcastEmail.m
+++ TeamSnapSDK/SDK/DataTypes/TSDKBroadcastEmail.m
@@ -7,6 +7,7 @@
 //
 
 #import "TSDKBroadcastEmail.h"
+#import "TSDKMember.h"
 
 @implementation TSDKBroadcastEmail
 
@@ -16,4 +17,52 @@
     return @"broadcast_email";
 }
 
+- (instancetype)initWithBody:(NSString *)body subject:(NSString *)subject teamId:(NSInteger)teamId recipients:(NSArray <TSDKMember *>*)recipients sender:(TSDKMember *)sender {
+    self = [super init];
+    if(self) {
+        [super setString:body forKey:@"body"];
+        [super setString:subject forKey:@"subject"];
+        [super setInteger:teamId forKey:@"team_id"];
+        [super setInteger:sender.objectIdentifier forKey:@"member_id"];
+        NSMutableArray *recipientIDs = [[NSMutableArray alloc] init];
+        for(TSDKMember *recipient in recipients) {
+            [recipientIDs addObject:[NSString stringWithFormat:@"%ld", (long)recipient.objectIdentifier]];
+        }
+        
+        [super setArray:recipientIDs forKey:@"recipient_ids"];
+    }
+    return self;
+}
+/*
+ 
+ "data": [{
+ "name": "attachments",
+ "value": null
+ }, {
+ "name": "body",
+ "value": null
+ }, {
+ "name": "from_email_address",
+ "value": null
+ }, {
+ "name": "is_draft",
+ "value": null
+ }, {
+ "name": "member_id",
+ "value": null
+ }, {
+ "name": "recipient_ids",
+ "value": null
+ }, {
+ "name": "subject",
+ "value": null
+ }, {
+ "name": "team_id",
+ "value": null
+ }, {
+ "name": "type",
+ "value": "broadcast_email"
+ }]
+ 
+ */
 @end
