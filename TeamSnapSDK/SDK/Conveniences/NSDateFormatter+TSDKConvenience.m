99d740a2196878d43d481590055650411e49db6d
Skyler Seamans <skyler.seamans@teamsnap.com>
2016-06-24 13:47:45 -0700

    Cache date formatters for performance

---
+++ TeamSnapSDK/SDK/Conveniences/NSDateFormatter+TSDKConvenience.m
@@ -0,0 +1,92 @@
+//
+//  NSDateFormatter+TSDKConvenience.m
+//  TeamSnapSDK
+//
+//  Created by Skyler Seamans on 6/24/16.
+//  Copyright © 2016 teamsnap. All rights reserved.
+//
+
+#import "NSDateFormatter+TSDKConvenience.h"
+
+@implementation NSDateFormatter (TSDKConvenience)
+
++ (NSDateFormatter *)RCF3339Style1DateFormatter {
+    static NSDateFormatter *dateFormatter = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        dateFormatter = [[NSDateFormatter alloc] init];
+        NSLocale *enUSPOSIXLocale = [[NSLocale alloc] initWithLocaleIdentifier:@"en_US_POSIX"];
+        
+        [dateFormatter setLocale:enUSPOSIXLocale];
+        [dateFormatter setTimeZone:[NSTimeZone timeZoneForSecondsFromGMT:0]];
+        
+        [dateFormatter setDateFormat:@"yyyy'-'MM'-'dd'T'HH':'mm':'ss'Z'"];
+    });
+    
+    return dateFormatter;
+}
++ (NSDateFormatter *)RCF3339Style2DateFormatter {
+    static NSDateFormatter *dateFormatter = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        dateFormatter = [[NSDateFormatter alloc] init];
+        NSLocale *enUSPOSIXLocale = [[NSLocale alloc] initWithLocaleIdentifier:@"en_US_POSIX"];
+        
+        [dateFormatter setLocale:enUSPOSIXLocale];
+        [dateFormatter setTimeZone:[NSTimeZone timeZoneForSecondsFromGMT:0]];
+        
+        [dateFormatter setDateFormat:@"yyyy'-'MM'-'dd'T'HH':'mm':'ssZZZ"];
+    });
+    
+    return dateFormatter;
+}
++ (NSDateFormatter *)RCF3339Style3DateFormatter {
+    static NSDateFormatter *dateFormatter = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        dateFormatter = [[NSDateFormatter alloc] init];
+        NSLocale *enUSPOSIXLocale = [[NSLocale alloc] initWithLocaleIdentifier:@"en_US_POSIX"];
+        
+        [dateFormatter setLocale:enUSPOSIXLocale];
+        [dateFormatter setTimeZone:[NSTimeZone timeZoneForSecondsFromGMT:0]];
+        
+        [dateFormatter setDateFormat:@"yyyy'-'MM'-'dd'T'HH':'mm':'ss'.'SSSZZZ"];
+    });
+    
+    return dateFormatter;
+
+}
++ (NSDateFormatter *)yearMonthDateFormatter {
+    static NSDateFormatter *dateFormatter = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        dateFormatter = [[NSDateFormatter alloc] init];
+        NSLocale *enUSPOSIXLocale = [[NSLocale alloc] initWithLocaleIdentifier:@"en_US_POSIX"];
+        
+        [dateFormatter setLocale:enUSPOSIXLocale];
+        [dateFormatter setTimeZone:[NSTimeZone timeZoneForSecondsFromGMT:0]];
+        
+        [dateFormatter setDateFormat:@"yyyy-MM-dd"];
+    });
+    
+    return dateFormatter;
+
+}
++ (NSDateFormatter *)monthDateYearFormatter {
+    static NSDateFormatter *dateFormatter = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        dateFormatter = [[NSDateFormatter alloc] init];
+        NSLocale *enUSPOSIXLocale = [[NSLocale alloc] initWithLocaleIdentifier:@"en_US_POSIX"];
+        
+        [dateFormatter setLocale:enUSPOSIXLocale];
+        [dateFormatter setTimeZone:[NSTimeZone timeZoneForSecondsFromGMT:0]];
+        
+        [dateFormatter setDateFormat:@"MMM d, yyyy"];
+    });
+    
+    return dateFormatter;
+
+}
+
+@end
