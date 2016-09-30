f73568baf1bc5e4af03c597d774bd129037829d1
Skyler Seamans <skyler.seamans@teamsnap.com>
2016-09-30 14:30:23 -0400

    utilize short label when appropriate on Events

--- TeamSnapSDK/SDK/DataTypes/TSDKEvent.m
+++ TeamSnapSDK/SDK/DataTypes/TSDKEvent.m
@@ -140,7 +140,7 @@
     }
 }
 
-- (NSString *)displayNameWithOpponent:(TSDKOpponent *)opponent {
+- (NSString *)displayNameWithOpponent:(TSDKOpponent *)opponent preferShortLabel:(BOOL)preferShortLabel {
     if (self.isGame && opponent) {
         if ([[self.gameType uppercaseString] isEqualToString:@"AWAY"]) {
             if ((self.label) && (![self.label isEqualToString:@""])) {
@@ -158,6 +158,8 @@
     } else {
         if (!self.name || (self.name.length == 0)) {
             return NSLocalizedString(@"Event", nil);
+        } else if(preferShortLabel && self.label.length) {
+            return self.label;
         } else {
             return self.name;
         }
