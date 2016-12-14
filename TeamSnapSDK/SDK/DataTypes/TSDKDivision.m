a60d59fb793214deeb2a17de278a1b03f2b85443
Skyler Seamans <skyler.seamans@teamsnap.com>
2016-12-14 15:21:50 -0600

    Added Division object

---
+++ TeamSnapSDK/SDK/DataTypes/TSDKDivision.m
@@ -0,0 +1,19 @@
+//
+//  TSDKDivision.m
+//  TeamSnapSDK
+//
+//  Created by Skyler Seamans on 12/14/16.
+//  Copyright © 2016 teamsnap. All rights reserved.
+//
+
+#import "TSDKDivision.h"
+
+@implementation TSDKDivision
+
+@dynamic sportId, leagueUrl, isArchived, postalCode, timeZoneOffset, country, updatedAt, leftBoundary, rightBoundary, parentId, isDeletable, rootId, timeZoneIanaName, isDisabled, name, planId, leagueName, timeZoneDescription, seasonName, locationCountry, timeZone, isAncestorArchived, createdAt, parentDivisionName, billingAddress, linkParent, linkChildren, linkAncestors, linkDescendants, linkRegistrationForms, linkPlan, linkTeams, linkDivisionEvents, linkDivisionPreferences;
+
++ (NSString *)SDKType {
+    return @"division";
+}
+
+@end
