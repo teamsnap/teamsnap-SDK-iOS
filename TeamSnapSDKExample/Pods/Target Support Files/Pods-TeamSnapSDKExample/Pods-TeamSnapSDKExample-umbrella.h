cf245e7103a701e5164725269b75db7a3f59ceda
Mark Gallegos <mark.gallegos@teamsnap.com>
2017-03-14 08:33:38 -0700

    Pod setup

---
+++ TeamSnapSDKExample/Pods/Target Support Files/Pods-TeamSnapSDKExample/Pods-TeamSnapSDKExample-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_TeamSnapSDKExampleVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_TeamSnapSDKExampleVersionString[];
+
---
+++ TeamSnapSDKExample/Pods/Target Support Files/TeamSnapSDK/TeamSnapSDK-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_TeamSnapSDK : NSObject
+@end
+@implementation PodsDummy_TeamSnapSDK
+@end
