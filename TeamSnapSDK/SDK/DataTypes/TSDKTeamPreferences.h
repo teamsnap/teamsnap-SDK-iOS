f9cc5d59c6d65940dd03555dbea0bd945566e204
Mark Gallegos <mark.gallegos@teamsnap.com>
2017-10-12 11:07:59 -0700

    Add canDisplayTeamStore property to team preferences

--- TeamSnapSDK/SDK/DataTypes/TSDKTeamPreferences.h
+++ TeamSnapSDK/SDK/DataTypes/TSDKTeamPreferences.h
@@ -78,6 +78,7 @@ typedef enum : NSUInteger {
 @property (nonatomic, weak) NSString *_Nullable ageGroup; //Example: **NULL**
 @property (nonatomic, assign) BOOL canTeamAddMembers;
 @property (nonatomic, assign) BOOL canTeamDeleteMembers;
+@property (nonatomic, assign) BOOL canDisplayTeamStore;
 
 @property (nonatomic, weak) NSString *_Nullable currencySymbol; //Example: $
 @property (nonatomic, assign) NSInteger hideHeader; //Example: 0
