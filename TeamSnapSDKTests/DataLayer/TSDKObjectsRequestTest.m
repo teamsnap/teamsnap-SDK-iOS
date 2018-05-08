fc628722eeea3d6f41194f3a9221b3febd950865
Jason Rahaim <jason@jasonr.com>
2018-05-08 13:53:46 -0400

    Test cleanup

--- TeamSnapSDKTests/DataLayer/TSDKObjectsRequestTest.m
+++ TeamSnapSDKTests/DataLayer/TSDKObjectsRequestTest.m
@@ -22,17 +22,7 @@
 
 @implementation TSDKObjectsRequestTest
 
-- (void)setUp {
-    [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
-}
-
-- (void)tearDown {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
-    [super tearDown];
-}
-
-- (void)testsupportedSDKObjects {    
+- (void)testsupportedSDKObjects {
     NSMutableArray *staticListA = [NSMutableArray arrayWithArray:[TSDKObjectsRequest supportedSDKObjects]];
     
     NSMutableArray *dynamicListA = [NSMutableArray arrayWithArray:[TSDKObjectsRequest dynamicSupportedObjectsList]];
@@ -49,13 +39,6 @@
     [dynamicListA removeObjectsInArray:staticListA];
     
     XCTAssertTrue(dynamicListA.count == 0, @"Missing classes in supportedSDKObjects: \n%@", dynamicListA);
-}
-
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
-    }];
 }
 
 @end
