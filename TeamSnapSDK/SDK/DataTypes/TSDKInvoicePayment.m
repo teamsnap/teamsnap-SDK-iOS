f3629736fb8a9416d31b0042a847a2b7bc9ae21b
Skyler Seamans <skylerseamans@gmail.com>
2018-06-20 18:01:38 -0800

    Add new processing fee information to invoice payment object.

--- TeamSnapSDK/SDK/DataTypes/TSDKInvoicePayment.m
+++ TeamSnapSDK/SDK/DataTypes/TSDKInvoicePayment.m
@@ -10,7 +10,7 @@
 
 @implementation TSDKInvoicePayment
 
-@dynamic detail, initialPaymentProviderName, isRefundable, initialPaymentProviderId, amount, amountWithCurrency, amountWithProcessingFeeWithCurrency, scheduledAt, createdAt, lastTransactedAt, updatedAt, invoiceId, status, amountWithProcessingFee, linkInvoice, linkTeam, linkInvoicePaymentTransactions;
+@dynamic detail, initialPaymentProviderName, isRefundable, initialPaymentProviderId, amount, amountWithCurrency, amountWithProcessingFeeWithCurrency, processingFeeWithCurrency, scheduledAt, createdAt, lastTransactedAt, updatedAt, invoiceId, status, amountWithProcessingFee, linkInvoice, linkTeam, linkInvoicePaymentTransactions;
 
 + (NSString *)SDKType {
     return @"invoice_payment";
