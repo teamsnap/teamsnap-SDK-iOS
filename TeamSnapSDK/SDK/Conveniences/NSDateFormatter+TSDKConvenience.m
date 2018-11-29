e6ce9838dbe88e0a94579915b6b074ead026dc7d
Skyler Seamans <skylerseamans@gmail.com>
2018-11-29 13:50:57 -0800

    Force Gregorian calendar on date formatter.

--- TeamSnapSDK/SDK/Conveniences/NSDateFormatter+TSDKConvenience.m
+++ TeamSnapSDK/SDK/Conveniences/NSDateFormatter+TSDKConvenience.m
@@ -61,7 +61,7 @@
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
         dateFormatter = [[NSDateFormatter alloc] init];
-        
+        dateFormatter.calendar = [NSCalendar calendarWithIdentifier:NSCalendarIdentifierGregorian];
         [dateFormatter setDateFormat:@"yyyy-MM-dd"];
     });
     
