e6c4b6d82a21c29737e637dc33b89528bf77d5da
Skyler Seamans <skylerseamans@gmail.com>
2019-03-06 09:30:24 -0600

    Change partner preference Int to Bool values

--- TeamSnapSDK/SDK/DataTypes/TSDKPartnerPreferences.h
+++ TeamSnapSDK/SDK/DataTypes/TSDKPartnerPreferences.h
@@ -7,10 +7,10 @@
 
 @interface TSDKPartnerPreferences : TSDKCollectionObject
 
-@property (nonatomic, assign) NSInteger canDisplayPartner; //Example: 0
+@property (nonatomic, assign) BOOL canDisplayPartner; //Example: 0
 @property (nonatomic, strong, nullable) NSString * partnerName; //Example: **NULL**
 @property (nonatomic, strong, nullable) NSString * userId; //Example: <null>
-@property (nonatomic, assign) NSInteger userIsPartner; //Example: 0
+@property (nonatomic, assign) BOOL userIsPartner; //Example: 0
 @property (nonatomic, strong, nullable) NSString * partnerId; //Example: <null>
 
 @end
