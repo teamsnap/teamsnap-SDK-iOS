407e89e0d1b0cb5e578c1e4a79591a52ed20bec8
Lili Shi <lili@mojo.sport>
2024-08-14 15:34:39 -0700

    [IOS-434] Make TeamSnapSDK repo SPM compatible

---
+++ Package.swift
@@ -0,0 +1,15 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "TeamSnapSDK",
+    products: [
+        .library(name: "TeamSnapSDK", targets: ["TeamSnapSDK"])
+    ],
+    dependencies: [],
+    targets: [
+        .target(name: "TeamSnapSDK", path: "TeamSnapSDK")
+    ]
+)
