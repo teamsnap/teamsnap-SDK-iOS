19977754e85802ae0eab9dbcddf3ca03e3b522b1
Caleb Rudnicki <caleb.rudnicki@teamsnap.com>
2025-01-15 22:32:55 -0500

    [IOS-953] Remove My Health Check and Team Health Check

--- TeamSnapSDK/TeamSnapSDK.h
+++ TeamSnapSDK/TeamSnapSDK.h
@@ -110,7 +110,4 @@
 #import <TeamSnapSDK/TSDKPartnerPreferences.h>
 #import <TeamSnapSDK/TSDKStripeAccount.h>
 #import <TeamSnapSDK/TSDKSuggestedAssignment.h>
-#import <TeamSnapSDK/TSDKHealthCheckQuestionnaire.h>
-#import <TeamSnapSDK/TSDKHealthCheckQuestionnaireTemplate.h>
-#import <TeamSnapSDK/TSDKHealthCheckQuestionnaireTemplateQuestion.h>
 #import <TeamSnapSDK/TSPCache.h>
